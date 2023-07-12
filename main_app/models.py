d5f461318fb23155c3749835fa48dc129349eadc
kellims <kellisimon18@gmail.com>
2023-07-12 18:55:03 -0500

    new form added

--- main_app/models.py
+++ main_app/models.py
@@ -12,7 +12,7 @@ class Trip(models.Model):
     image = models.CharField(max_length=20000, default='default_image.html')
     
     def __str__(self):
-        return self.name
+        return self.location
 
     class Meta:
         ordering = ['start_date']
