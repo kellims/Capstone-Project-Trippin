f44c97b16594e6245a1971ece9967f9c48106389
kellims <kellisimon18@gmail.com>
2023-07-12 20:05:40 -0500

    delete url added

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('trips/new/', views.TripCreate.as_view(), name="trip_create"),
     path('trips/<int:pk>/', views.TripDetail.as_view(), name="trip_detail"),
     path('trips/<int:pk>/update',views.TripUpdate.as_view(), name="trip_update"),
+    path('trips/<int:pk>/delete',views.TripDelete.as_view(), name="trip_delete"),
 ]
