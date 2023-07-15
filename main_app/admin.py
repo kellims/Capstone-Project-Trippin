2c23181b7ddc77bb47aead1343c8311d12ca89ab
kellims <kellisimon18@gmail.com>
2023-07-15 11:46:48 -0500

    Added Reservations to trip page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Trip
+from .models import Trip, Reservation
 
 # Register your models here.
 
 admin.site.register(Trip)
+admin.site.register(Reservation)
