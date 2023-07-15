a5b3783f432519f400e90d912ab255f4a241566b
kellims <kellisimon18@gmail.com>
2023-07-15 11:33:55 -0500

    Reservation model added

--- main_app/models.py
+++ main_app/models.py
@@ -15,3 +15,43 @@ class Trip(models.Model):
 
     class Meta:
         ordering = ['start_date']
+
+
+
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(max_length=500)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    location = models.CharField(max_length=150)
+    file = models.URLField(max_length=1000, blank=True)
+
+    # Reviewed documention on how to add choices. Trying to add choices below:
+    DINING = 'Dining'
+    HOTEL = 'Hotel'
+    TRANSPORTATION = 'Transportation'
+    DAY_TRIP = 'Day Trip'
+    MUSEUM = 'Museum'
+    SITE_SEEING = 'Site Seeing'
+    TOUR = 'Tour'
+    OTHER = 'Other'
+
+    type_choices = [
+        (DINING, "Dining"),
+        (HOTEL, "Hotel"),
+        (TRANSPORTATION, "Transportation"),
+        (DAY_TRIP, "Day Trip"),
+        (MUSEUM, "Museum"),
+        (SITE_SEEING, "Site Seeing"),
+        (TOUR, "Tour"),
+        (OTHER, "Other"),
+    ]
+    type = models.CharField(choices=type_choices, max_length=150)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name="reservations")
+
+    def __str__(self):
+        return self.name        
+    
+    class Meta:
+        ordering = ['start_date']
