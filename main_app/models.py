0e73332990a4378ce2ed2ed10852e33c38d8a151
kellims <kellisimon18@gmail.com>
2023-07-18 16:39:06 -0500

    migrated Models update for user

--- main_app/models.py
+++ main_app/models.py
@@ -9,6 +9,7 @@ class Trip(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     image = models.CharField(max_length=20000, default='default_image.html')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     
     def __str__(self):
         return self.location
