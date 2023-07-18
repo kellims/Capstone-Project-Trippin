923b0d023ccc558e4ed7bd31b8568fc2934d8486
kellims <kellisimon18@gmail.com>
2023-07-18 17:00:09 -0500

    update views

--- main_app/views.py
+++ main_app/views.py
@@ -29,7 +29,7 @@ class TripList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["trips"] = Trip.objects.all()
+        context["trips"] = Trip.objects.filter(user=self.request.user)
         return context
     
 class TripCreate(CreateView):
