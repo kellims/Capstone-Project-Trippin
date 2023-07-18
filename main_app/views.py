1db708055d80199158eaeb3398ace4b6c11fb753
kellims <kellisimon18@gmail.com>
2023-07-18 17:06:11 -0500

    Added signup view

--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
 from django.urls import reverse
 from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 import logging
 
@@ -119,4 +121,20 @@ class ReservationDelete(DeleteView):
     template_name = "reservation_delete_confirmation.html"
     
     def get_success_url(self):
-        return reverse('reservation_detail', kwargs={'pk': self.object.pk})     
+        return reverse('reservation_detail', kwargs={'pk': self.object.pk})   
+
+
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("trip_list")
+        else:
+            context = {"form": form}
+            return render(request, "registration/signup.html", context)
